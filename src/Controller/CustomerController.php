fd22a24e558c5d2b3f6f28ec52cb70f64403dd97
Bruno Ricardo Siqueira <brunoric@gmail.com>
2018-06-24 22:25:04 +0100

    Adding customer controller

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the php-poc-geoapi console application.
+ *
+ * (c) Bruno Ricardo Siqueira <brunoric@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\RepositoryInterface;
+
+/**
+ * Class CustomerController
+ * @package App\Controller
+ */
+class CustomerController
+{
+    protected $logger;
+    protected $repository;
+
+    public function __construct(LoggerInterface $logger, RepositoryInterface $repository)
+    {
+        $this->logger = $logger;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @Route("customer/list/{property}/{direction}")
+     * @param string|null $property
+     * @param string|null $direction
+     * @return Response
+     */
+    public function list(?string $property = null, ?string $direction = null)
+    {
+        $customers = $this->repository->fetchAll($property, $direction);
+
+        return new JsonResponse($customers);
+    }
+}
---
+++ tests/Controller/CustomerControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of the php-poc-geoapi console application.
+ *
+ * (c) Bruno Ricardo Siqueira <brunoric@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Tests\Controller;
+
+use Psr\Log\NullLogger;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CustomerControllerTest extends WebTestCase
+{
+    protected $logger;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->logger = new NullLogger();
+    }
+
+    public function criteriaProvider()
+    {
+        return [
+            ['name', 'asc'],
+            ['name', 'desc'],
+            ['id', 'asc'],
+            ['id', 'desc'],
+            ['latitude', 'asc'],
+            ['latitude', 'desc'],
+            ['longitude', 'asc'],
+            ['longitude', 'desc'],
+        ];
+    }
+
+    /**
+     * This is a functional test which
+     *
+     * @dataProvider criteriaProvider
+     * @param string $property
+     * @param string $order
+     */
+    public function testList($property, $order)
+    {
+        $client = static::createClient();
+        $client->request('GET', "/customer/list/$property/$order");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
